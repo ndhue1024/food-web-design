a16aa6a3bb27a89c9fe7b566cf514a56ae0b9537
ndhue1024 <huend@cozwork.com>
2023-12-27 17:31:45 +0700

    about button

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,11 +5,11 @@ import './style.css'
 
 const About = () => {
   return (
-    <>
+    <div className="about-page">
       <AboutComponent img="https://res.cloudinary.com/dpb8gwksd/image/upload/v1703579013/food-web/about-page_oieqdn.png" />
       <AboutContent1 />
       <AboutContent2 />
-    </>
+    </div>
   )
 }
 
